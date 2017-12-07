970b4d0a93f462c44e37185431af37083639af7d
nicolasmanic <nicolas.kanakis@gmail.com>
2017-12-08 00:25:12 +0200

    Question 5.5 (sort of)

---
+++ src/Bit_Manipulation/Q5.java
@@ -0,0 +1,21 @@
+package Bit_Manipulation;
+
+/**
+ * Created by Nicolas on 7/12/2017.
+ */
+public class Q5 {
+    /*
+    *  A & B == 0 only when A =~B (there is no position in A and B that have 1s
+    *
+    *  Since A = n & B = n-1 the only way that the above equation is true is when n has only one 1
+    *  eg: n = 1 = 1
+    *      n = 10 = 2
+    *      n = 100 = 4
+    *      n = 10000 = 8
+    *      n = 100000 = 16
+    *      n = 1000000 = 32
+    *      ....
+    *
+    *  So this code checks if n is a power of 2
+    * */
+}
