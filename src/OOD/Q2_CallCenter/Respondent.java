b0dde964fbc46b18a52ad99b5f7892759af07480
nicolasmanic <nicolas.kanakis@gmail.com>
2017-12-14 19:41:36 +0200

    Respondent type of Employee

---
+++ src/OOD/Q2_CallCenter/Respondent.java
@@ -0,0 +1,10 @@
+package OOD.Q2_CallCenter;
+
+/**
+ * Respondent type of Employee.
+ */
+public class Respondent extends Employee {
+    public Respondent(){
+        rank = Rank.Respondent;
+    }
+}
