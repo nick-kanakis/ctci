f713c5332f479ef7f8e6d05ff7af1ab6d8d556eb
nicolasmanic <nicolasmanic@gmail.com>
2017-12-17 10:57:11 +0200

    Puzzle Solution Algorithm

---
+++ src/OOD/Q6_Puzzle/SolutionAlgo.java
@@ -0,0 +1,22 @@
+package OOD.Q6_Puzzle;
+
+
+import java.util.LinkedList;
+
+public class SolutionAlgo {
+
+    public boolean solve(){
+        // Group together  CornerPieces, BorderPieces, InsidePieces
+
+        //Iterate the solution grid row by row and select the correct list of the above 3 available to search for.
+
+        //pass the list and the position to the fitNextEdge method
+
+        return false;
+    }
+
+    public boolean fitNextEdge(LinkedList<Piece> piecesToSearchFrom, int column, int row){
+        // find correct piece that matches the exact left.
+        return false;
+    }
+}
